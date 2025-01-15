35166c97c473ef56615dd879ce1d9a612a20568e
priya-prasad-dev <priyaprasad.pd@gmail.com>
2025-01-15 09:03:00 +0530

    swagger done

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import adminRoutes from "./src/routes/admin.routes.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import employeeRoutes from "./src/routes/employee.routes.js";
 import managerRoutes from "./src/routes/manager.routes.js";
+import swaggerDocument from "./src/swagger-docs/spec.json" assert { type: "json" };
 dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/employee", employeeRoutes);
