4dd80213f3c078bd5c7052058120a6973225f6f8
priya-prasad-dev <priyaprasad.pd@gmail.com>
2025-01-23 10:38:23 +0530

    modified code

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,43 +1,23 @@
 import { verifyToken } from "../utils/auth.js";
 import { errorHandler } from "../utils/responseHandler.js";
 
-// Admin role verification middleware
-export const isAdmin = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.role !== "ADMIN") {
-      console.error("Access denied: User does not have admin privileges.");
-      return errorHandler(new Error("You do not have admin access"), 403, res);
-    }
-    next();
-  });
+export const verifyRole = (requiredRole) => {
+  return (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user.role !== requiredRole) {
+        console.error(`Access denied: User does not have ${requiredRole} privileges.`);
+        return errorHandler(
+          new Error(`You do not have ${requiredRole.toLowerCase()} access`),
+          403,
+          res
+        );
+      }
+      next();
+    });
+  };
 };
 
-// Manager role verification middleware
-export const isManager = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.role !== "MANAGER") {
-      console.error("Access denied: User does not have manager privileges.");
-      return errorHandler(
-        new Error("You do not have manager access"),
-        403,
-        res
-      );
-    }
-    next();
-  });
-};
-
-// Employee role verification middleware
-export const isEmployee = (req, res, next) => {
-  verifyToken(req, res, () => {
-    if (req.user.role !== "EMPLOYEE") {
-      console.error("Access denied: User does not have employee privileges.");
-      return errorHandler(
-        new Error("You do not have employee access"),
-        403,
-        res
-      );
-    }
-    next();
-  });
-};
+// Specific role middlewares
+export const isAdmin = verifyRole("ADMIN");
+export const isManager = verifyRole("MANAGER");
+export const isEmployee = verifyRole("EMPLOYEE");
